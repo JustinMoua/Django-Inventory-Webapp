537b5b4ed050dcac80c3ec7e705bd6c3ff9285d9
Justin Moua <39628304+JustinMoua@users.noreply.github.com>
2025-04-14 01:49:19 -0500

    Installed jquery and jquery.redirect.

--- infraTieSite/djangoApp/models.py
+++ infraTieSite/djangoApp/models.py
@@ -10,7 +10,7 @@ class Inspection(models.Model):
     Attribute_7 = models.CharField(max_length=50)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.id
 
     class Meta:
         db_table = "Inspection"
